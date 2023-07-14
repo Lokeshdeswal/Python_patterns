fc33ffa23ff3868e0dcba9a32e33749f0ea3d750
Lokesh <lokeshdeswal2000.gmail.com>
2023-07-14 22:26:51 +0530

    pattern 5

---
+++ Pattern5/pat5.py
@@ -0,0 +1,24 @@
+n=4
+i=1
+while i<=n:
+    s=chr(ord('A')+i-1)
+    j=1
+    while j<=i:
+        p=chr(ord(s)+j-1)
+        print(p,end='')
+        j+=1
+    print()
+    i+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
